8fd946026e354c918b631cfec94f1090d6e06c47
Sean Maden <metamaden@users.noreply.github.com>
2022-01-14 17:08:53 -0800

    rm extraneous code

--- series/generative/smpte/src/pix.py
+++
@@ -1,9 +0,0 @@
-# source: https://www.tensorflow.org/tutorials/generative/pix2pix
-
-import tensorflow as tf
-import os
-import pathlib
-import time
-import datetime
-from matplotlib import pyplot as plt
-from IPython import display
